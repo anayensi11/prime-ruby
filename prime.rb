f9c06cff40f3e27917e46f68241aac79d53ae5ca
anayensi <vnvyensi@gmail.com>
2021-01-14 20:38:05 -0500

    commit tree 833314e8252ccc33bf6f74ab6d2f188eb3704e65

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
 # Add  code here!
+def returns true for prime numbers
+      expect(prime?(2)).to be(true)
+      expect(prime?(3)).to be(true)	
+      expect(prime?(11)).to be(true)
+      expect(prime?(105557)).to be(true)
+end
